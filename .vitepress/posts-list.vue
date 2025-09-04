3675fe0da778c941e2da5a7afc08024dd1d282ae
uyamazak <uyamazak@gmail.com>
2025-09-04 10:18:03 +0900

    update

--- .vitepress/posts-list.vue
+++ .vitepress/posts-list.vue
@@ -2,9 +2,9 @@
 <ul class="post-list">
   <li v-for="post of posts" :key="post.url" class="post-item">
     <a :href="post.url" class="post-link">
-      <span class="post-date">{{ formatDate(post.frontmatter.published) }}</span>
-      <span v-if="post.frontmatter.categoryLabel" class="category-label">{{ post.frontmatter.categoryLabel }}</span>
+      <span v-if="post.frontmatter.categoryLabel" class="category">{{ post.frontmatter.categoryLabel }}</span>
       <span class="title">{{ post.frontmatter.title }}</span>
+      <span class="post-date">{{ formatDate(post.frontmatter.published) }}</span>
     </a>
   </li>
 </ul>
@@ -17,7 +17,6 @@ const props = defineProps({
     required: true
   }
 })
-
 </script>
 <style scoped>
 ul.post-list {
@@ -41,11 +40,12 @@ ul.post-list {
   background-color: #333;
 }
 .post-date {
-  flex-shrink: 0; /* 日付部分が縮まないようにする */
-  margin-right: .5em;
-  color: #666;
-  /* 等幅フォント */
-  font-family: 'Courier New', Courier, monospace;
+  font-size: .75rem;
+  padding-left: .5rem;
+  color: #777;
+  font-family: 'Space Mono', 'Roboto Mono', 'Noto Sans Mono', monospace;
+  font-variant-numeric: tabular-nums;
+  font-weight: lighter;
 }
 .dark .post-date {
   color: #ccc;
